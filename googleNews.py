e147724eb197f16b8d5777ce0753b81080e92303
황승원 <kevin1113dev@gmail.com>
2024-04-09 15:25:48 +0900

    edit googleNews.py

--- googleNews.py
+++ googleNews.py
@@ -7,10 +7,27 @@ from selenium import webdriver
 
 # Get links from google news
 
-
 def getDateAndLinks(keyword, term):
     url = 'https://news.google.com/rss/search?q=' + keyword + \
         '+when:' + str(term) + 'd&hl=en-US&gl=US&ceid=US:en'
+    text = getData(url)
+    datas = feedparser.parse(text).entries
+    links = []
+    driver = webdriver.Chrome()
+    for data in datas:
+        date = data.published
+        driver.get(data.link)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        linkTag = soup.find('a')
+        if linkTag == None:
+            continue
+        links.append([date, linkTag.text])
+    driver.quit()
+    return links
+
+def getDateAndLinks(keyword, t1, t2):
+    url = 'https://news.google.com/rss/search?q=' + keyword + \
+        '+after:' + t1 + '+before:' + t2 + '&ceid=US:en&hl=en-US&gl=US'
     text = getData(url)
     datas = feedparser.parse(text).entries
     links = []
@@ -59,6 +76,19 @@ def getNews(keyword, term):
         articles.append(str(link[0]) + "\n\n" + article)
     return articles
 
+def getNews(keyword, t1, t2):
+    g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
+              'parser_class': 'soup', 'strict': False})
+    links = getDateAndLinks(keyword, t1, t2)
+    articles = []
+    for link in links:
+        # print(link)
+        article = getArticle(link[1], g)
+        if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
+            continue
+        articles.append(str(link[0]) + "\n\n" + article)
+    return articles
+
 # Parse time from string
 #   ex) "Tue, 02 Apr 2024 13:07:16 GMT"
 #         -> time.struct_time(tm_year=2024, tm_mon=4, tm_mday=2, tm_hour=13, tm_min=7, tm_sec=16, tm_wday=1, tm_yday=93, tm_isdst=-1)
@@ -92,7 +122,7 @@ def parseTime(timeStr):
 # -----------------
 # | 파일로 저장하기 2 |
 # -----------------
-articles = getNews('TSLA', 1)
+articles = getNews('TSLA', "2024-04-07", "2024-04-08")
 for index in range(len(articles)):
-    with open('./articles/' + str(index+1) + '.txt', 'w') as outfile:
+    with open('./test/' + str(index+1) + '.txt', 'w') as outfile:
         outfile.write(articles[index])
