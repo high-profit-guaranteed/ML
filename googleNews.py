f768533601c5f35d8b662c311c0c0f163096b8d3
황승원 <kevin1113dev@gmail.com>
2024-04-10 18:40:25 +0900

    update googleNews.py

--- googleNews.py
+++ googleNews.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from datetime import datetime
 import time
 import requests
 import feedparser
@@ -7,24 +9,25 @@ from selenium import webdriver
 
 # Get links from google news
 
-def getDateAndLinks(keyword, term):
-    url = 'https://news.google.com/rss/search?q=' + keyword + \
-        '+when:' + str(term) + 'd&hl=en-US&gl=US&ceid=US:en'
-    text = getData(url)
-    datas = feedparser.parse(text).entries
-    links = []
-    driver = webdriver.Chrome()
-    for data in datas:
-        date = data.published
-        driver.get(data.link)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        linkTag = soup.find('a')
-        if linkTag == None:
-            continue
-        links.append([date, linkTag.text])
-    driver.quit()
-    return links
+# def getDateAndLinks(keyword, term):
+#     url = 'https://news.google.com/rss/search?q=' + keyword + \
+#         '+when:' + str(term) + 'd&hl=en-US&gl=US&ceid=US:en'
+#     text = getData(url)
+#     datas = feedparser.parse(text).entries
+#     links = []
+#     driver = webdriver.Chrome()
+#     for data in datas:
+#         date = data.published
+#         driver.get(data.link)
+#         soup = BeautifulSoup(driver.page_source, 'html.parser')
+#         linkTag = soup.find('a')
+#         if linkTag == None:
+#             continue
+#         links.append([date, linkTag.text])
+#     driver.quit()
+#     return links
 
+# https://www.google.com/search?q=google+search+date&client=safari&sca_esv=3d20c04c2adbaa4c&sca_upv=1&rls=en&sxsrf=ACQVn090P0KZuMNbSEHive6QuI_YyEFBaw%3A1712733717852&source=lnt&tbs=cdr%3A1%2Ccd_min%3A4%2F2%2F2024%2Ccd_max%3A4%2F10%2F2024&tbm=
 def getDateAndLinks(keyword, t1, t2):
     url = 'https://news.google.com/rss/search?q=' + keyword + \
         '+after:' + t1 + '+before:' + t2 + '&ceid=US:en&hl=en-US&gl=US'
@@ -42,6 +45,24 @@ def getDateAndLinks(keyword, t1, t2):
         links.append([date, linkTag.text])
     driver.quit()
     return links
+
+# def getDateAndLinks(keyword):
+#     url = 'https://news.google.com/rss/search?q=' + keyword + \
+#         '&ceid=US:en&hl=en-US&gl=US'
+#     text = getData(url)
+#     datas = feedparser.parse(text).entries
+#     links = []
+#     driver = webdriver.Chrome()
+#     for data in datas:
+#         date = data.published
+#         driver.get(data.link)
+#         soup = BeautifulSoup(driver.page_source, 'html.parser')
+#         linkTag = soup.find('a')
+#         if linkTag == None:
+#             continue
+#         links.append([date, linkTag.text])
+#     driver.quit()
+#     return links
 
 # Get data from link
 
@@ -63,30 +84,66 @@ def getArticle(link, g):
 # Get news from keyword and term
 
 
-def getNews(keyword, term):
-    g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
-              'parser_class': 'soup', 'strict': False})
-    links = getDateAndLinks(keyword, term)
-    articles = []
-    for link in links:
-        # print(link)
-        article = getArticle(link[1], g)
-        if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
-            continue
-        articles.append(str(link[0]) + "\n\n" + article)
-    return articles
+# def getNews(keyword, term):
+#     g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
+#               'parser_class': 'soup', 'strict': False})
+#     links = getDateAndLinks(keyword, term)
+#     articles = []
+#     for link in links:
+#         # print(link)
+#         article = getArticle(link[1], g)
+#         if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
+#             continue
+#         articles.append(str(link[0]) + "\n\n" + article)
+#     return articles
 
-def getNews(keyword, t1, t2):
+# def getNews(keyword, t1, t2):
+#     g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
+#               'parser_class': 'soup', 'strict': False})
+#     links = getDateAndLinks(keyword, t1, t2)
+#     articles = []
+#     for link in links:
+#         # print(link)
+#         article = getArticle(link[1], g)
+#         if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
+#             continue
+#         articles.append(str(link[0]) + "\n\n" + article)
+#     return articles
+
+# def getNews(keyword):
+#     g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
+#               'parser_class': 'soup', 'strict': False})
+#     links = getDateAndLinks(keyword)
+#     articles = []
+#     for link in links:
+#         # print(link)
+#         article = getArticle(link[1], g)
+#         if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
+#             continue
+#         articles.append(str(link[0]) + "\n\n" + article)
+#     return articles
+
+def getNews(keyword, tS1, tS2):
     g = Goose({'browser_user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2)',
               'parser_class': 'soup', 'strict': False})
-    links = getDateAndLinks(keyword, t1, t2)
+    # t1 = datetime.fromtimestamp(time.mktime(time.strptime(tS1, "%Y-%m-%d")))
+    # t2 = datetime.fromtimestamp(time.mktime(time.strptime(tS2, "%Y-%m-%d")))
+    t1 = datetime.strptime(tS1, "%Y-%m-%d")
+    t2 = datetime.strptime(tS2, "%Y-%m-%d")
     articles = []
-    for link in links:
-        # print(link)
-        article = getArticle(link[1], g)
-        if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
-            continue
-        articles.append(str(link[0]) + "\n\n" + article)
+    while t1 <= t2:
+        timeStr1 = t1.strftime("%Y-%m-%d")
+        timeStr2 = (t1 + timedelta(days=1)).strftime("%Y-%m-%d")
+        print(t1, timeStr1)
+        links = getDateAndLinks(keyword, timeStr1, timeStr2)
+        print(links)
+        for link in links:
+            # print(link)
+            article = getArticle(link[1], g)
+            if article == "" or article == None or article == "Please enable JS and disable any ad blocker" or article == "Keep me logged in from this computer." or article.startswith("Access to this page has been denied because we believe you are using automation tools to browse the website."):
+                continue
+            articles.append(str(link[0]) + "\n\n" + article)
+        t1 += timedelta(days=2)
     return articles
 
 # Parse time from string
@@ -122,7 +179,16 @@ def parseTime(timeStr):
 # -----------------
 # | 파일로 저장하기 2 |
 # -----------------
-articles = getNews('TSLA', "2024-04-07", "2024-04-08")
+articles = getNews('TSLA', "2023-04-05", "2024-04-05")
 for index in range(len(articles)):
-    with open('./test/' + str(index+1) + '.txt', 'w') as outfile:
+    with open('./test4/' + str(index+1) + '.txt', 'w') as outfile:
         outfile.write(articles[index])
+
+
+# # -----------------
+# # | 파일로 저장하기 3 |
+# # -----------------
+# articles = getNews('TSLA')
+# for index in range(len(articles)):
+#     with open('./test2/' + str(index+1) + '.txt', 'w') as outfile:
+#         outfile.write(articles[index])
